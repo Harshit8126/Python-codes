e15b5927a4112bc1d56b260ec98c6ed3c95a9282
Harshit9410 <46756130+Harshit9410@users.noreply.github.com>
2019-01-16 22:32:52 +0530

    Add files via upload

---
+++ largest no .py
@@ -0,0 +1,9 @@
+x=int(input('enter the first number:'))
+y=int(input('enter the second number:'))
+z=int(input('enter the third number:'))
+if x>y and x>z:
+    print('x is largest')
+elif y>z:
+    print(' y is largest')
+else:
+    print('z is largest')
