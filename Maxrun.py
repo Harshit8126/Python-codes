0dfa278f0f797bd6ddfe19b08e55df2549d32ecc
Harshit9410 <46756130+Harshit9410@users.noreply.github.com>
2019-01-16 23:09:27 +0530

    Add files via upload

---
+++ Maxrun.py
@@ -0,0 +1,6 @@
+#program to find max run that a player made in a cricket match:
+over=int(input('enter the no of overs:'))
+maxrun=(33*(over-1))+36
+print('Maxrun=',maxrun)
+         
+               
