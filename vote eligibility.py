5e61d5259963f5037ba31d1ff6c02e3d123ab30e
Harshit9410 <46756130+Harshit9410@users.noreply.github.com>
2019-01-18 21:47:21 +0530

    Add files via upload

---
+++ vote eligibility.py
@@ -0,0 +1,6 @@
+#program to find whether the person is eligible for vote or not:---
+age=int(input("enter the age of a person:"))
+if age>=18:
+    print('person is eligible for vote.')
+else:
+    print('person is not eligible for vote.')
