7d402a0a0cb634f6c89d26e542011eb9f77885e8
Harshit9410 <46756130+Harshit9410@users.noreply.github.com>
2019-01-18 22:24:01 +0530

    Add files via upload

---
+++ check whether the no is +ve,-ve,zero.py
@@ -0,0 +1,9 @@
+#program to check whether the no is positive ,negative or zero:----
+num=int(input('enter the number'))
+if num>0:
+     print('positive number')
+elif num<0:
+     print('negative number')
+else:
+     print('zero')
+        
