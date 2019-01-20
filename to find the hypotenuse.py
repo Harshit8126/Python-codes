a8de5d6536968c9bb8a27ebab4eae2d06f3acd0b
Harshit9410 <46756130+Harshit9410@users.noreply.github.com>
2019-01-20 21:31:19 +0530

    Program to find the hypotenuse of a triangle.

---
+++ to find the hypotenuse.py
@@ -0,0 +1,5 @@
+#program to find hypotenuse:----
+Base=int(input('enter the base:'))
+Perpendicular=int(input('enter the perprndicular:'))
+Hypotenuse=(((Base*Base)+(Perpendicular*Perpendicular))**0.5)
+print('Hypotenuse is  ',Hypotenuse)
