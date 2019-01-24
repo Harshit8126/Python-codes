3980dd71c2234070aab8ffc4037fca5eb4f52324
Harshit9410 <46756130+Harshit9410@users.noreply.github.com>
2019-01-24 20:29:58 +0530

    Program to find the sum of n natural no.

---
+++ sum upto n.py
@@ -0,0 +1,9 @@
+#Program to find the sum of n natural numbers:---
+n=int(input('enter the number:'))
+average = 0
+sum = 0
+for i in range(0,n+1):
+    sum = sum+i;
+
+print("sum of first ", n, "number is: ", sum)
+            
